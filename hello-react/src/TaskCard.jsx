47322e1e6d86d82a68b2fc7d95c61dc3a710b3b2
Jaideep-iare <21951a0567@iare.ac.in>
2024-10-10 00:12:33 +0530

    Level 1 of react

---
+++ hello-react/src/TaskCard.jsx
@@ -0,0 +1,13 @@
+import "./TaskCard.css"
+const TaskCard = (props) => {
+    console.log(props)
+    return (
+        <div className="TaskItem">
+          <h2 className="text-xl font-bold">{props.title}</h2>
+          <p>{(props.dueDate)?`Due on:${props.dueDate}`:`Completed on:${props.completedAtDate}`}</p>
+          <p>{props.assigneeName}</p>
+        </div>
+      )
+  }
+
+  export default TaskCard
